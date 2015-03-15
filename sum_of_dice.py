78e3b7c3423465928dd56fc8b415ad922f8a88e7
Gabriele Favalessa <favalex@gmail.com>
2015-03-15 19:56:24 +0100

    spelling error

--- sum_of_dice.py
+++ sum_of_dice.py
@@ -1,13 +1,13 @@
 from pmf import Pmf
 from functools import reduce
 
-class Dice(Pmf):
+class Die(Pmf):
 	def __init__(self, sides):
 		Pmf.__init__(self)
 		for side in range(1, 1+sides):
 			self.set(side, 1.0 / sides)
 
-three = Dice(6) + Dice(6) + Dice(6)
+three = Die(6) + Die(6) + Die(6)
 three.plot()
 
 print()
